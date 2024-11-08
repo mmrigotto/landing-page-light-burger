a15f012d91d56786ffe1fa9213031f22aeacaee9
mmrigotto <mmrigotto@gmail.com>
2024-11-08 20:25:33 -0300

    feito icons vantagens e inicio mais vendidos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { IconsComponent } from './components/icons/icons.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { VendidosComponent } from './components/vendidos/vendidos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    IconsComponent,
+    CardsComponent,
+    VendidosComponent
   ],
   imports: [
     BrowserModule
